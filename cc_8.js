dc2bdff428b5692c1eebb04d1e71da561ec0e6f4
Jairo Roa <Jroa@usf.edu>
2025-02-16 21:56:05 -0500

    Task 2 - Product Price After Discount.

--- cc_8.js
+++ cc_8.js
@@ -6,3 +6,12 @@ function calculateSalary(baseSalary, bonus, taxRate) {
 }
 calculateSalary(5000, 500, 0.1);
 calculateSalary(7000, 1000, 0.15);
+
+// Task 2: Function Expression
+const calculateDiscount = function(price, discountRate) {
+    const finalPrice = price - (price * discountRate);
+    console.log(`Final Price: $${finalPrice.toFixed(2)}`);
+}
+
+calculateDiscount(100, 0.2);
+calculateDiscount(250, 0.15);
